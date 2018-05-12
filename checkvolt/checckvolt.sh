0878715f4b5be3877879e09c422fa6f1573561a0
DdraigX <6390496+DdraigX@users.noreply.github.com>
2018-05-12 10:48:36 -0400

    Create checckvolt.sh

---
+++ checkvolt/checckvolt.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+tail -fn0 /var/log/syslog | \
+while read line ; do
+        echo "$line" | grep "Under-volage"
+        if [ $? = 0 ]
+        then
+                echo "Time: $(date) !!Detected Under-Voltage" >> /home/pi/voltlog.log
+                sudo shutdown -h now #Modify this line to add additional commands. 
+        fi
+done
