2d53c19cb59ea24d74a2b531cddb0a9df5cb6ac3
ddraig@fdwsnetworks.com <ddraig@fdwsnetworks.com@users.noreply.github.com>
2020-10-28 12:05:47 -0400

    Updated log location

--- checkvolt/checkvolt.sh
+++ checkvolt/checkvolt.sh
@@ -6,7 +6,7 @@ while read line ; do
         echo "$line" | grep "Under-voltage"
         if [ $? = 0 ]
         then
-                echo "Time: $(date) !!Detected Under-Voltage" >> /home/pi/voltlog.log
+                echo "Time: $(date) !!Detected Under-Voltage" >> /var/log/voltlog.log
                 xmessage -center -timeout 60 "UnderVoltage, Check Battery, Powering Down"
                 sleep 10
                 sudo shutdown -h now #Modify this line to add additional commands. 
